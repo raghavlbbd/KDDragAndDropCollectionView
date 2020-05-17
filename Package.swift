d594150781c1735060a17dfe00be4e582fbea0cb
raghavlbbd <48044789+raghavlbbd@users.noreply.github.com>
2020-05-17 19:23:35 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,23 +2,13 @@ import PackageDescription
 
 let package = Package(
     name: "KDDragAndDropCollectionView",
-    platforms: [
-        .iOS(.v10),
-    ],
     products: [
-        .library(
-            name: "KDDragAndDropCollectionView",
-            targets: ["KDDragAndDropCollectionView"]),
-    ],
-    dependencies: [
-        // no dependencies
-    ],
-    targets: [
-        .target(
-            name: "KDDragAndDropCollectionView",
-            dependencies: []),
-        .testTarget(
-            name: "KDDragAndDropCollectionViewTests",
-            dependencies: ["KDDragAndDropCollectionView"]),
-    ]
+       .library(name: "KDDragAndDropCollectionView", targets: ["KDDragAndDropCollectionView"])
+   ],
+   targets: [
+       .target(
+           name: "KDDragAndDropCollectionView",
+           path: "KDDragAndDropCollectionView"
+       )
+   ]
 )
